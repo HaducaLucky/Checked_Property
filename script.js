db10cf923d0ae0f4272a744245aeb4f545f60a38
HaducaLucky <147017329+HaducaLucky@users.noreply.github.com>
2024-03-20 19:10:31 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function(){
+
+    if(myCheckBox.checked){
+        subResult.textContent = `You are subscribe!`;
+    }
+    else{
+        subResult.textContent = `You are NOT subscribe!`;
+    }
+
+    if(visaBtn.checked){
+        paymentResult.textContent = `You are paying with Visa`;
+    }
+    else if(masterCardBtn.checked){
+        paymentResult.textContent = `You are paying with Master Card`;
+    }
+    else if(payPalBtn.checked){
+        paymentResult.textContent = `You are paying with PayPal`;
+    }
+    else{
+        paymentResult.textContent = `You must select a payment type`;
+    }
+}
